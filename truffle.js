465ba8c8442e0feb8832acfb38d72af3174d553e
Jimmy Chu <jimmychu0807@gmail.com>
2018-11-18 18:19:12 +0800

    new commit

--- truffle.js
+++ truffle.js
@@ -1,5 +1,10 @@
 module.exports = {
   networks: {
+    local_dev: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
     development: {
       host: "localhost",
       port: 8545,
